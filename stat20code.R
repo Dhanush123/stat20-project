788d033d5f71188559cbf7438f6af3397d5e13c5
Dhanush Patel <dhanush.patel@ymail.com>
2018-12-05 18:50:57 -0800

    updated EDA

--- stat20code.R
+++ stat20code.R
@@ -7,11 +7,20 @@ top10 = head(data2016[order(data2016$happiness, decreasing=TRUE),], 10)
 bottom10 = head(data2016[order(data2016$happiness, decreasing=FALSE),], 10)
 combo = rbind(top10,bottom10)
 combo$country = str_wrap(combo$country, width = 10)
-topGraph = ggplot(data=combo,
+finalGraph = ggplot(data=combo,
           aes(x=reorder(country,-happiness),y=happiness,fill=happiness))+
           labs(title="Top and Bottom 10 Countries by Happiness in 2016")+
           labs(subtitle="Note: Happiness is more formally known as \"Life Ladder\"")+
+          labs(caption="By Dhanush Patel")+
           xlab("Country")+
           ylab("Happiness")+
           geom_bar(stat="identity")
-topGraph
+finalGraph
+biggestDiff = head(combo,1)$happiness-tail(combo,1)$happiness
+biggestDiff
+topAvg = sum(head(combo,5)$happiness)/5
+botAvg = sum(tail(combo,5)$happiness)/5
+topAvg
+botAvg
+diffAvg = topAvg - botAvg
+diffAvg
